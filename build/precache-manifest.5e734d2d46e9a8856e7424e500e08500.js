42c0b4c3c5dcb48175835815fdc54c7c9e50044a
yadavshashwat <y.shashwat@gmail.com>
2021-06-07 00:19:12 +0530

    React Version Correction

---
+++ build/precache-manifest.5e734d2d46e9a8856e7424e500e08500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e57807f8443fbbd589c",
+    "url": "/static/css/main.f61b84f3.chunk.css"
+  },
+  {
+    "revision": "8e57807f8443fbbd589c",
+    "url": "/static/js/main.721e4937.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36c3d4eaeedcde29a1be",
+    "url": "/static/css/2.9b7e4c5a.chunk.css"
+  },
+  {
+    "revision": "36c3d4eaeedcde29a1be",
+    "url": "/static/js/2.489798ce.chunk.js"
+  },
+  {
+    "revision": "4fd854f384d178af0f9d4500ee5e7e14",
+    "url": "/index.html"
+  }
+];
